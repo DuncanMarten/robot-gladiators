db1e3aa6a34bcd1dc988d3a08035aea7ec398b65
Duncan <duncanmarten@yahoo.com>
2020-09-14 16:26:04 -0500

    added round numbers

--- assets/js/game.js
+++ assets/js/game.js
@@ -92,11 +92,18 @@ var fight = function(enemyName) {
 
 // For loop to fight each robot
 for(var i = 0; i < enemyNames.length; i++) {
-    //debugger;
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
+    //debugger;
     // call fight function
     fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
 // Execute fight function
 //fight();
