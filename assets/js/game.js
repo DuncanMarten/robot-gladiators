9b9e54e67b8229bf4cc1c1421fd02600a70b10ee
Duncan <duncanmarten@yahoo.com>
2020-09-15 10:59:20 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,7 +36,7 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 // subtract money
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
@@ -45,7 +45,9 @@ var fight = function(enemyName) {
         // if player chose fight
         if (promptFight === "fight" || promptFight === "FIGHT") {
             // Subtract playerAttack from enemyHealth
-            enemyHealth = enemyHealth - playerAttack;
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+
+            enemyHealth = Math.max(0, enemyHealth - damage);
             console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
@@ -61,7 +63,9 @@ var fight = function(enemyName) {
             }
 
             // Subtract enemyAttack from playerHealth
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+            playerHealth = Math.max(0, playerHealth - damage);
             console.log(
                 enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
@@ -96,7 +100,7 @@ var startGame = function() {
 
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             //debugger;
 
@@ -194,6 +198,13 @@ var shop = function() {
             break;
     }
 };
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+  };
 
 // calls the start game function
 startGame();
